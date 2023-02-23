ff6545160c134f39cf390f4c00cd1007149f4dbd
SzymonKowalczyk1 <115708886+SzymonKowalczyk1@users.noreply.github.com>
2023-02-23 15:53:00 +0100

    1_2_7

---
+++ lab1/1_2_7/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Podaj liczbe: ");
+    int liczba;
+    scanf("%d",&liczba);
+    printf("Liczba: %d", liczba+1);
+    return 0;
+}
