26bf62dd5c952c8ce739c5d84d3e94131c3ab55f
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-01 13:10:20 +0100

    1_2_9

---
+++ lab1/1_2_9/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int liczba;
+
+    scanf("%i",&liczba);
+    printf("%f Pierwiastek z tej liczby: ", sqrt(liczba));
+    return 0;
+}
