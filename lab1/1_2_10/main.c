a58038e1b7597eebc352653f10676962773854e8
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-01 13:17:04 +0100

    1_2_10

---
+++ lab1/1_2_10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    double liczba;
+    scanf("%lf", &liczba);
+    double wynik = abs(liczba);
+    printf("%lf",wynik);
+    return 0;
+}
