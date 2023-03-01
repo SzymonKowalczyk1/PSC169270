68ac91f9b492a52eaf821a946ce09f8a6526c9c0
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-01 13:00:16 +0100

    1_2_8

--- lab1/1_2_8/main.c
+++ lab1/1_2_8/main.c
@@ -4,13 +4,13 @@
 int main()
 {
     printf("Podaj 3 liczby calkowite: ");
-    int a,b,c;
-    scanf("%d", &a);
-    scanf("%d", &a);
-    scanf("%d", &a);
+    float a,b,c;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
 
     float srednia;
-    srednia = (a + b + c) / 3;
+    srednia = (a + b + c) / 3.0;
      printf("Srednia artmeyczna tych liczb to: %f",srednia);
     return 0;
 }
