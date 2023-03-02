9a37e4eff82a58f0f58d8e29370e08b3bb981643
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-02 16:10:56 +0100

    1_3_8

---
+++ lab2/1_3_8/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    double a,b,h;
+    printf("Jakiej figury chcesz policzyc pole?\n");
+    printf("1 - kwadrat \n");
+    printf("2 - prostokat \n");
+    printf("3 - trojkat \n");
+    scanf("%i",x);
+    switch(x) {
+    case 1:
+        printf("Podaj bok kwadratu: ");
+        scanf("%f",a);
+        p = a*a;
+
+
+    }
+    return 0;
+}
