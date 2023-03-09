28780bc271a6a10997a8d2388ad6a40ce401c15f
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-09 15:34:17 +0100

    2_2_17

---
+++ lab3/2_2_17/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int liczba_wywolan = 0;
+
+void zadanie()
+{
+
+    liczba_wywolan++;
+    printf("Wywolanie nr: %d\n",liczba);
+
+
+}
+int main()
+{
+    zadanie();
+    zadanie();
+    zadanie();
+    zadanie();
+    zadanie();
+    return 0;
+}
