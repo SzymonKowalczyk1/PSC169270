016785e261738f5e0dcb5ed7d4dd0913ffd6b83e
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-16 14:56:37 +0100

    3_2_5

---
+++ lab4/3_2_5/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fun (int*x, int*y)
+{
+    return *x + *y;
+
+}
+int main()
+{
+    int x = 9, y = 2;
+
+    printf("%i\n",fun (&x,&y));
+    return 0;
+}
