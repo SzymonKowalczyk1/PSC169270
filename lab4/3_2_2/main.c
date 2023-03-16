d482cd302f395e558814db56f9032ca5b1f6a9e9
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-16 14:41:13 +0100

    3_2_2

--- lab4/3_2_2/main.c
+++ lab4/3_2_2/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-int fun(int *a, int *b)
+int* fun(int *a, int *b)
 {
     if (*a < *b)
-        return *a;
-    return *b;
+        return a;
+    return b;
 
 }
 int main()
 {
     int a = 10, b = 14;
-    printf("%d\n", fun(&a,&b));
+    printf("Adres a %p\n", &a);
+    printf("Adres b %p\n", &b);
+    printf("%p\n",fun(&a,&b));
     return 0;
 }
