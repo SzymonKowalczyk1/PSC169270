52c2c7a17e52f2884005c5ec715330c91f5420a9
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-22 20:32:27 +0100

    3_2_12

--- lab4/3_2_12/main.c
+++ lab4/3_2_12/main.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 double *fun (int n)
 {
-    double wsk* = malloc(n *sizeof(double));
+    double *wsk = malloc(n *sizeof(double));
     return wsk;
 }
 int main()
 {
-    int * wsk2 = foo(6);
+    double * wsk2 = fun(6);
     printf("%p\n",wsk2);
     return 0;
 }
