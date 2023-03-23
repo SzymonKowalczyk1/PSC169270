520ae584cd6c5e53d4580e7bf0b8f4d535b6c753
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-23 15:54:44 +0100

    3_2_13

---
+++ lab5/3_2_13/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double fun(double(*w)(int),int n)
+{
+return fun(a);
+}
+double pom(int arg)
+{
+    return (double)(arg+3);
+}
+
+int main()
+{
+    printf("%lf\n",foo(pom, 5));
+    return 0;
+}
