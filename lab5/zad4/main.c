c15200b663d0a7ef441abc0ef31bcf0c24789a97
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-03-23 16:08:05 +0100

    zad4

---
+++ lab5/zad4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int foo(const int *wsk1, int * const wsk2) {
+    return *wsk1 - *wsk2;
+}
+int main()
+{
+    int a = 5;
+    int b =3;
+    printf("%d\n",foo(&a,&b));
+    return 0;
+}
