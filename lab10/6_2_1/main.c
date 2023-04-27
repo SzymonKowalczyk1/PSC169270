987268b6b878f852c63cb6981b1a3c5cd610e1e5
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-04-27 14:25:25 +0200

    6_2_1

--- lab10/6_2_1/main.c
+++ lab10/6_2_1/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int ** alokuj(int n, int m)
+{
+    int ** temp = malloc(n*sizeof(int*));
+    for (int i =0; i<n; i++)
+    {
+        temp[i] = malloc(m*sizeof(int));
+    }
+    return temp;
+}
 int main()
 {
-    printf("Hello world!\n");
+    int **tab = alokuj(2,3);
+    printf("%p\n", tab);
     return 0;
 }
