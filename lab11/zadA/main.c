72d0a89dd8f7210cd9d298d68216669b9a9cf193
SzymonKowalczyk <szymon.kowalczyk12@gmail.com>
2023-05-18 11:34:40 +0200

    lab11

---
+++ lab11/zadA/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Osoba
+{
+    char imie[20];
+    int wiek;
+};
+
+struct Osoba initOsoba(char im[50], int wk)
+{
+    struct Osoba temp;
+    temp.wiek =wk;
+    int i=0;
+    for(i=0;im[i] !=0; i++)
+    {
+        temp.imie[i] = im[i];
+    }
+    temp.imie[i]=0;
+    return temp;
+};
+
+void pokazOsoba (struct Osoba arg)
+{
+    printf("Imie: %s, wiek: %d\n", arg.imie, arg.wiek);
+}
+
+void urodziny(struct Osoba * wsk)
+{
+    wsk->wiek++;
+}
+
+int main()
+{
+    struct Osoba nowa = initOsoba("Stefan",35);
+    pokazOsoba(nowa);
+    urodziny(&nowa);
+    pokazOsoba(nowa);
+    return 0;
+}
